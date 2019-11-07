1bd0150380bc7c733b448d9df91b9d353dac9e5b
Majid <majidlotfinia12@gmail.com>
2019-11-07 11:33:25 +0330

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+import { NativeModules } from 'react-native';
+
+const { DeviceSecurity } = NativeModules;
+
+export default DeviceSecurity;
---
+++ ios/DeviceSecurity.h
@@ -0,0 +1,5 @@
+#import <React/RCTBridgeModule.h>
+
+@interface DeviceSecurity : NSObject <RCTBridgeModule>
+
+@end
