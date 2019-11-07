7c5c146d230cf19c6c0f313459b628af25b0f255
Majid <majidlotfinia12@gmail.com>
2019-11-07 14:56:04 +0330

    add device rooted method

--- android/src/main/java/com/device/security/DeviceSecurityModule.java
+++ android/src/main/java/com/device/security/DeviceSecurityModule.java
@@ -5,7 +5,14 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+
 import android.provider.Settings;
+
+import android.os.Build;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
 
 public class DeviceSecurityModule extends ReactContextBaseJavaModule {
 
@@ -35,5 +42,44 @@ public class DeviceSecurityModule extends ReactContextBaseJavaModule {
                     Settings.Global.ADB_ENABLED, 0);
         }
         promise.resolve(adb == 1);
+    }
+
+    /**
+     * Checks if the device is rooted.
+     *
+     * @return <code>true</code> if the device is rooted, <code>false</code> otherwise.
+     */
+    @ReactMethod
+    public void isRooted(Promise promise) {
+        promise.resolve(checkRootMethod1() || checkRootMethod2() || checkRootMethod3());
+        return;
+    }
+
+    private static boolean checkRootMethod1() {
+        String buildTags = android.os.Build.TAGS;
+        return buildTags != null && buildTags.contains("test-keys");
+    }
+
+    private static boolean checkRootMethod2() {
+        String[] paths = { "/system/app/Superuser.apk", "/sbin/su", "/system/bin/su", "/system/xbin/su", "/data/local/xbin/su", "/data/local/bin/su", "/system/sd/xbin/su",
+                "/system/bin/failsafe/su", "/data/local/su", "/su/bin/su"};
+        for (String path : paths) {
+            if (new File(path).exists()) return true;
+        }
+        return false;
+    }
+
+    private static boolean checkRootMethod3() {
+        Process process = null;
+        try {
+            process = Runtime.getRuntime().exec(new String[] { "/system/xbin/which", "su" });
+            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            if (in.readLine() != null) return true;
+            return false;
+        } catch (Throwable t) {
+            return false;
+        } finally {
+            if (process != null) process.destroy();
+        }
     }
 }
